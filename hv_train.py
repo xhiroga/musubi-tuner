b830b35f9a1cc11ee79abed27675a2f8536dd9ec
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-05-27 09:20:38 +0000

    feat: added wandb to .gitignore and modified hv_train.py to include epoch numbers in logging

--- hv_train.py
+++ hv_train.py
@@ -1148,8 +1148,8 @@ class FineTuningTrainer:
                     break
 
             if len(accelerator.trackers) > 0:
-                logs = {"loss/epoch": loss_recorder.moving_average}
-                accelerator.log(logs, step=epoch + 1)
+                logs = {"loss/epoch": loss_recorder.moving_average, "epoch": epoch + 1}
+                accelerator.log(logs)
 
             accelerator.wait_for_everyone()
 
